b49881990c08421e5280847ba8ba05a718534271
Florian Weber <florian.weber@ueberbit.de>
2015-10-23 10:14:56 +0200

    Simplify build script

--- build/build.sh
+++ build/build.sh
@@ -5,27 +5,8 @@ cd build
 composer install --no-interaction
 rm -rf build-7.x build-8.0.x
 
-if [ ! -d "build-7.x" ]; then
-  git clone --branch 7.x $REPO build-7.x
-  if [ ! -d "build-7.x" ]; then
-    git clone $REPO build-7.x
-    cd build-7.x
-    git checkout --orphan 7.x
-    git rm --cached -- *
-    cd ..
-  fi
-fi
-
-if [ ! -d "build-8.0.x" ]; then
-  git clone --branch 8.0.x $REPO build-8.0.x
-  if [ ! -d "build-8.0.x" ]; then
-    git clone $REPO build-8.0.x
-    cd build-8.0.x
-    git checkout --orphan 8.0.x
-    git rm --cached -- *
-    cd ..
-  fi
-fi
+git clone --branch 7.x $REPO build-7.x
+git clone --branch 8.0.x $REPO build-8.0.x
 
 php build-composer-json.php
 
