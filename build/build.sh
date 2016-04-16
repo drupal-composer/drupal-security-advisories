0453d95459eaa1797562bc7f0eb252570b6c5320
Florian Weber <florian.weber@ueberbit.de>
2016-04-16 16:20:41 +0200

    Fix build script

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@
 
 REPO="https://github.com/drupal-composer/drupal-security-advisories.git"
 cd build
-composer install --no-interaction
+composer install --no-interaction --no-progress
 rm -rf build-7.x build-8.0.x
 
 git clone --branch 7.x $REPO build-7.x
@@ -11,13 +11,19 @@ git clone --branch 8.0.x $REPO build-8.0.x
 php build-composer-json.php
 
 cd build-7.x
-git add composer.json
-git commit -m "Update composer.json"
-git push origin HEAD
+if [ ! -z "$(git status --porcelain)" ]
+then
+  git add composer.json
+  git commit -m "Update composer.json"
+  git push origin HEAD
+fi
 cd ..
 
 cd build-8.0.x
-git add composer.json
-git commit -m "Update composer.json"
-git push origin HEAD
+if [ ! -z "$(git status --porcelain)" ]
+then
+  git add composer.json
+  git commit -m "Update composer.json"
+  git push origin HEAD
+fi
 cd ..
