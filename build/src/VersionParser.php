c1d5b739143a029964eb64eaf17d29713b02d701
Moshe Weitzman <weitzman@tejasa.com>
2020-06-29 09:24:41 -0400

    Keep prior constraints for non-semver

--- build/src/VersionParser.php
+++ build/src/VersionParser.php
@@ -35,8 +35,21 @@ class VersionParser {
     // $result->taxonomy_vocabulary_6 is usually a term like 8.x (https://www.drupal.org/taxonomy/term/7234).
     // Its absence indicates a semver release (or a core release).
     list($core, $version) = empty($result->taxonomy_vocabulary_6) ? [NULL, $version] : explode('-', $version, 2);
-    list($major, $minor) = explode('.', $version, 2);
-    return ">=$major.$minor,<$version";
+    $version_components = explode('.', $version);
+    if (count($version_components) === 2) {
+      // Only major.minor, either Drupal core 7, or contrib that had an API compatibility prefix.
+      list($major) = $version_components;
+      return ">=$major,<$version";
+    }
+    elseif (count($version_components) === 3) {
+      // Semver, either Drupal core 8 or later, or contrib using semver.
+      list($major, $minor) = $version_components;
+      return ">=$major.$minor,<$version";
+    }
+    else {
+      // Should not happen. An exception will be thrown by the caller.
+      return NULL;
+    }
   }
 
   public static function isValid($version) {
