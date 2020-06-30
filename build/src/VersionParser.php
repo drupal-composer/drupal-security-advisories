dc5232a4bcf0f88bb3eab27a871980ce9bd9881a
Moshe Weitzman <weitzman@tejasa.com>
2020-06-30 15:13:33 -0400

    Handle D9 core SAs and handle semver contrib releases (#22)
    * Handle D9 core SAs and handle Contrib releases
    
    * Update README.md
    
    Co-authored-by: Neil Drumm <drumm@delocalizedham.com>
    
    * Include minor in constraint for Contrib
    
    * Keep prior constraints for non-semver
    
    Co-authored-by: Neil Drumm <drumm@delocalizedham.com>

--- build/src/VersionParser.php
+++ build/src/VersionParser.php
@@ -4,14 +4,14 @@ namespace DrupalComposer\DrupalSecurityAdvisories;
 
 class VersionParser {
 
-  public static function generateRangeConstraint($version, $isCore) {
+  public static function generateRangeConstraint($version, $isCore, $result) {
     if (!static::isValid($version)) {
       return FALSE;
     }
-    return $isCore ? static::handleCore($version) : static::handleContrib($version);
+    return $isCore ? static::handleCore($version) : static::handleContrib($version, $result);
   }
 
-  public static function generateExplicitConstraint($version, $isCore) {
+  public static function generateExplicitConstraint($version, $isCore, $result) {
     if (!static::isValid($version)) {
       return FALSE;
     }
@@ -19,7 +19,9 @@ class VersionParser {
       return $version;
     }
     else {
-      list($core, $version) = explode('-', $version, 2);
+      // $result->taxonomy_vocabulary_6 is usually a term like 8.x (https://www.drupal.org/taxonomy/term/7234).
+      // Its absence indicates a semver release (or a core release).
+      list($core, $version) = empty($result->taxonomy_vocabulary_6) ? [NULL, $version] : explode('-', $version, 2);
     }
     return $version;
   }
@@ -29,10 +31,25 @@ class VersionParser {
     return ">=$major.$minor,<$version";
   }
 
-  public static function handleContrib($version) {
-    list($core, $version) = explode('-', $version, 2);
-    list($major) = explode('.', $version);
-    return ">=$major,<$version";
+  public static function handleContrib($version, $result) {
+    // $result->taxonomy_vocabulary_6 is usually a term like 8.x (https://www.drupal.org/taxonomy/term/7234).
+    // Its absence indicates a semver release (or a core release).
+    list($core, $version) = empty($result->taxonomy_vocabulary_6) ? [NULL, $version] : explode('-', $version, 2);
+    $version_components = explode('.', $version);
+    if (count($version_components) === 2) {
+      // Only major.minor, either Drupal core 7, or contrib that had an API compatibility prefix.
+      list($major) = $version_components;
+      return ">=$major,<$version";
+    }
+    elseif (count($version_components) === 3) {
+      // Semver, either Drupal core 8 or later, or contrib using semver.
+      list($major, $minor) = $version_components;
+      return ">=$major.$minor,<$version";
+    }
+    else {
+      // Should not happen. An exception will be thrown by the caller.
+      return NULL;
+    }
   }
 
   public static function isValid($version) {
